fc49193437e089002c0583d242efaf135efa2a62
peixinxin <aa20040523@gmail.com>
2023-12-05 20:48:29 +0800

    1

--- DS_Project/src/Main.java
+++ DS_Project/src/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
 		String c;
 		String dee;
-		String e;
+		String e; 
 		int bbb = 531;
 		String abcd;
 		int aaa=528;
 		int ccc=534;
 		
-		String error
+		
 
 		System.out.println(a.indexOf("rld", 4));
 	}
