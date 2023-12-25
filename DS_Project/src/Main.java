4701d9871d6a108ce6e3253d6144bef121a0b6d1
peixinxin <aa20040523@gmail.com>
2023-12-25 14:42:39 +0800

    111

--- DS_Project/src/Main.java
+++ DS_Project/src/Main.java
@@ -1,23 +1,10 @@
-import java.io.IOException;
-import java.util.*;
-
-import org.w3c.dom.UserDataHandler;
 
 public class Main {
-	public static void main(String[] args) {
-		try {
-			
-			Scanner userScanner = new Scanner(System.in);
-			System.out.println("User input: ");
-			String userInput = userScanner.nextLine();
-			
-			System.out.println(new GoogleQuery(userInput).query());
-//			System.out.println(new GoogleQuery("�f�X").query());
-			userScanner.close();
-		} 
-		catch (IOException e) {
-			
-			e.printStackTrace();
-		}
+
+	public static void main(String[]args) {
+	  	String aString;
+
 	}
+	
 }
+
