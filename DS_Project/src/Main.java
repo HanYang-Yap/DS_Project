70a7ff91f999974fb6e045460c7e159a3698a715
HanYang-Yap <117017994+HanYang-Yap@users.noreply.github.com>
2023-12-25 14:32:26 +0800

    2:32pm Main class

--- DS_Project/src/Main.java
+++ DS_Project/src/Main.java
@@ -1,11 +1,23 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.w3c.dom.UserDataHandler;
 
 public class Main {
-
-	public static void main(String[]args) {
-		
-	  	String aString;
-
+	public static void main(String[] args) {
+		try {
+			
+			Scanner userScanner = new Scanner(System.in);
+			System.out.println("User input: ");
+			String userInput = userScanner.nextLine();
+			
+			System.out.println(new GoogleQuery(userInput).query());
+//			System.out.println(new GoogleQuery("�f�X").query());
+			userScanner.close();
+		} 
+		catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
-	
 }
-
