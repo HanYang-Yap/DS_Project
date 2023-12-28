57a84cae71f786c41dfe639e3ff5037ad06af349
peixinxin <aa20040523@gmail.com>
2023-12-28 16:07:08 +0800

    0407

--- DS_Project/src/WebComparator.java
+++ DS_Project/src/WebComparator.java
@@ -2,20 +2,28 @@ import java.util.Comparator;
 
 public class WebComparator implements Comparator<WebNode> {
 	
+//	public int compare(WebNode o1, WebNode o2) {
+//		
+//		if (o1 == null || o2 == null)
+//			throw new NullPointerException();
+//		
+//		if (o1.nodeScore < o2.nodeScore) {
+//			return 1;
+//			
+//		} else if (o1.nodeScore > o2.nodeScore) {
+//			return -1;
+//		
+//		} else {
+//			 return 0;
+//		}
+	
 	public int compare(WebNode o1, WebNode o2) {
-		
-		if (o1 == null || o2 == null)
-			throw new NullPointerException();
-		
-		if (o1.nodeScore < o2.nodeScore) {
-			return 1;
-			
-		} else if (o1.nodeScore > o2.nodeScore) {
-			return -1;
-		
-		} else {
-			 return 0;
-		}
+	    if (o1 == null || o2 == null)
+	        throw new NullPointerException();
 
+	    return Integer.compare((int)o1.nodeScore,(int) o2.nodeScore);
 	}
+
+
+
 }
--- DS_Project/src/WebTree.java
+++ DS_Project/src/WebTree.java
@@ -15,58 +15,17 @@ public class WebTree
 	public WebTree(ArrayList<WebNode> queue) throws IOException
 	{
 		//this.root = new WebNode(rootPage);
+		
 		this.heap = new PriorityQueue<WebNode>(queue.size(), new WebComparator());
 		this.queue = queue;
 		for(WebNode w : queue) {
 			this.add(w);
 			w.setNodeSocre();
 		}
-		reorder();
+		
 	
 	}
-	public void reorder() {
-        ArrayList<WebNode> origin = new ArrayList<>();
-        ArrayList<WebNode> tempList = new ArrayList<>();
-        int index = 0;
-        WebNode max = origin.get(0);
-        for(WebNode w : queue) {
-        	if(w.score>max.score) {
-        		origin.add(origin.indexOf(max)-1);
-        	}
-        	
-        }
-        while (!origin.isEmpty()) {
-            for(int i=0; i<origin.size();i++) {
-            	
-            }
-        	
-        }
-        
-    }
-	
-	
-	
-	public void reorderHeap() {
-        ArrayList<WebNode> tempList = new ArrayList<>();
 
-        while (!heap.isEmpty()) {
-            tempList.add(heap.poll());
-        }
-
-        // 使用 Collections.sort 並傳入分數的比較器
-        Collections.sort(tempList, new ScoreComparator());
-
-        heap.addAll(tempList);
-    }
-
-    private static class ScoreComparator implements Comparator<WebNode> {
-        @Override
-        public int compare(WebNode node1, WebNode node2) {
-            // 根據分數由大到小排序
-            return Integer.compare((int)node2.score,(int) node1.score);
-        }
-    }
-	
 	public void add(WebNode k)
 	{
 		heap.offer(k);
