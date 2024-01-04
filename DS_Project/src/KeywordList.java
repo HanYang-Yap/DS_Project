275917197f81eb403655df7a8bd75d9a9cec5de1
Zhanzii9624 <sophiajeng88@gmail.com>
2024-01-04 13:33:31 +0800

    update keyword list

--- DS_Project/src/KeywordList.java
+++ DS_Project/src/KeywordList.java
@@ -13,14 +13,23 @@ public class KeywordList
 	public void setKword() {
 		
 		keywords.add(new Keyword("HIKING TRAIL", 5));
+		keywords.add(new Keyword("步道", 4));
 		keywords.add(new Keyword("SCENARY", 3));
+		keywords.add(new Keyword("景色", 3));
+		keywords.add(new Keyword("景觀", 3));
 		keywords.add(new Keyword("NATIONAL PARK", 3));
+		keywords.add(new Keyword("國家公園", 3));
 		keywords.add(new Keyword("FOREST", 3));
+		keywords.add(new Keyword("森林", 3));
 		keywords.add(new Keyword("CLIMB", 3));
 		keywords.add(new Keyword("GUIDE", 3));
 		keywords.add(new Keyword("HIKING", 3));
+		keywords.add(new Keyword("攀爬", 3));
+		keywords.add(new Keyword("登頂", 3));
+		keywords.add(new Keyword("高峰", 3));
 		keywords.add(new Keyword("NATURE", 4));
 		keywords.add(new Keyword("100 MOUNTAIN", 2));
+		keywords.add(new Keyword("百岳", 2));
 		keywords.add(new Keyword("EXERCISE", 1));
 		keywords.add(new Keyword("ATTRACTION", 4));
 		keywords.add(new Keyword("PUBLIC TRANSPORTATION", 2));
